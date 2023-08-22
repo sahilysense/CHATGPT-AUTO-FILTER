2398bff298877b3c9ee400bfdb2030884cb60018
sahilysense <142903426+sahilysense@users.noreply.github.com>
2023-08-22 21:34:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://Popindiaxbot:<password>@cluster0.9uhus0k.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Popindiaxbot:sahil@8989@cluster0.9uhus0k.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = 'Telegram_files'
 
